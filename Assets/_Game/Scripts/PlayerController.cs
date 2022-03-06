32ae249b3d77006ffeb441fea22b5dcd64e48083
fahricanelidemir@gmail.com <>
2022-03-06 06:59:54 +0100

    Basic top down movement

---
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 4f;
+
+    private float _inputHorizontal;
+    private float _inputVertical;
+    private Rigidbody2D _rb;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        _inputHorizontal = Input.GetAxisRaw("Horizontal");
+        _inputVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        var velocity = new Vector2(_inputHorizontal, _inputVertical);
+        _rb.velocity = velocity.normalized * walkSpeed;
+    }
+}
