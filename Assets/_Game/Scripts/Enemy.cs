d118cba55b40c5611a7e2072153009e396f4e34d
fahricanelidemir@gmail.com <>
2022-03-07 14:18:54 +0100

    NavMeshPlus and an improved enemy

--- Assets/_Game/Scripts/Enemy.cs
+++ Assets/_Game/Scripts/Enemy.cs
@@ -6,8 +6,11 @@ public class Enemy : MonoBehaviour
 {
     private GameObject _player;
     private Rigidbody2D _rb;
+    private bool _isShooting = false;
 
-    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float walkSpeed = 2f;
+    [SerializeField] private float chaseRange = 5f;
+    [SerializeField] private float shootRange = 7f;
     
     // Start is called before the first frame update
     void Start()
@@ -19,7 +22,25 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var direction = _player.transform.position - transform.position;
-        _rb.transform.Translate(direction * (walkSpeed * Time.deltaTime));
+        var distanceToPlayer = Vector2.Distance(_player.transform.position, transform.position);
+        if (!_isShooting)
+        {
+            if (distanceToPlayer <= chaseRange)
+            {
+                _rb.velocity = Vector2.zero;
+                _isShooting = true;
+            }
+            else
+            {
+                var direction = (_player.transform.position - transform.position).normalized;
+                _rb.velocity = direction * walkSpeed;
+            }
+            
+        }
+        else
+        {
+            if (distanceToPlayer > shootRange)
+                _isShooting = false;
+        }
     }
 }
