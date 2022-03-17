5327e857227554409d0fc489c8caecd3a5fd1679
fahricanelidemir@gmail.com <>
2022-03-17 15:47:01 +0100

    Mapping tool and cinemachine setup

--- Assets/_Game/Scripts/GameManager.cs
+++ Assets/_Game/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,16 +8,25 @@ public class GameManager : MonoBehaviour
 {
 
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject followCamera;
 
     [SerializeField] private Vector2 spawnPosition;
     // Start is called before the first frame update
     void Awake()
     {
         SpawnPlayer();
+        AttachCamera();
     }
 
     private void SpawnPlayer()
     {
-        Instantiate(player, spawnPosition, Quaternion.identity);
+        player = Instantiate(player, spawnPosition, Quaternion.identity);
+    }
+    
+    
+    private void AttachCamera()
+    {
+        var cinemachine = followCamera.GetComponent<CinemachineVirtualCamera>();
+        cinemachine.Follow = player.transform;
     }
 }
